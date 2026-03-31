4fee43459707c7d34f8aa729bfc7c182d0df8a82
DarkPhoenix2704 <anbarasun123@gmail.com>
2026-03-31 15:05:18 +0000

    fix: file reference and audit fixes

--- packages/nocodb/src/db/BaseModelSqlv2.ts
+++ packages/nocodb/src/db/BaseModelSqlv2.ts
@@ -5081,9 +5081,12 @@ class BaseModelSqlv2 implements IBaseModelSqlV2 {
   }
 
   public async afterBulkRestore(data: any, req): Promise<void> {
-    const parentAuditId = await Noco.ncAudit.genNanoid(MetaTable.AUDIT);
+    const isBulk = data?.length > 1;
+    const parentAuditId = isBulk
+      ? await Noco.ncAudit.genNanoid(MetaTable.AUDIT)
+      : undefined;
 
-    if (await this.isDataAuditEnabled()) {
+    if (isBulk && (await this.isDataAuditEnabled())) {
       await Audit.insert(
         await generateAuditV1Payload<DataBulkDeletePayload>(
           AuditV1OperationTypes.DATA_BULK_RESTORE,
@@ -5100,7 +5103,7 @@ class BaseModelSqlv2 implements IBaseModelSqlV2 {
         ),
       );
     }
-    req.ncParentAuditId = parentAuditId;
+    if (parentAuditId) req.ncParentAuditId = parentAuditId;
 
     const column_meta = extractColsMetaForAudit(this.model.columns);
 

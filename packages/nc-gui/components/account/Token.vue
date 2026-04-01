ee1b9e03fee66ef74a6f2fdd823080448b77386b
Ramesh Mane <101566080+rameshmane7218@users.noreply.github.com>
2026-04-01 05:52:21 +0000

    chore: lint

--- packages/nc-gui/components/account/Token.vue
+++ packages/nc-gui/components/account/Token.vue
@@ -283,9 +283,9 @@ const handleCancel = () => {
               <span class="py-3.5 pl-2 text-nc-content-gray-muted font-medium text-3.5 w-3/9 text-start" data-rec="true">{{
                 $t('labels.token')
               }}</span>
-              <span class="py-3.5 pl-5 lg:pl-19 text-nc-content-gray-muted font-medium text-3.5 w-2/9 text-start" data-rec="true">{{
-                $t('labels.actions')
-              }}</span>
+              <span class="py-3.5 pl-5 lg:pl-19 text-nc-content-gray-muted font-medium text-3.5 w-2/9 text-start" data-rec="true">
+                {{ $t('labels.actions') }}
+              </span>
             </div>
             <div class="nc-scrollbar-md !overflow-y-auto flex flex-col h-[calc(100%-52px)]">
               <div v-if="showNewTokenModal">
@@ -347,9 +347,9 @@ const handleCancel = () => {
                 <span class="text-nc-content-gray-extreme font-bold text-3.5 text-start w-2/9">
                   <div class="flex items-center gap-2">
                     <NcTooltip class="truncate" show-on-truncate-only>
-                    <template #title>
-                      {{ el.description }}
-                    </template>
+                      <template #title>
+                        {{ el.description }}
+                      </template>
                       {{ el.description }}
                     </NcTooltip>
                     <NcTooltip v-if="el.fk_sso_client_id" placement="top">
@@ -363,7 +363,7 @@ const handleCancel = () => {
                     <template #title>
                       {{ el.created_by }}
                     </template>
-                      {{ el.created_by }}
+                    {{ el.created_by }}
                   </NcTooltip>
                 </span>
                 <span class="pl-2 text-nc-content-gray-muted font-medium text-3.5 text-start w-3/9 truncate">
